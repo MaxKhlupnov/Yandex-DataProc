5211ca862050761c1df5e24cb8b72d268b0125e6
khlupnov <m.khlupnov@gmail.com>
2020-06-21 10:09:34 +0300

    Connection strings and config settings

---
+++ src/main/java/Load/SecretsConst.java
@@ -0,0 +1,15 @@
+package Load;
+
+public final class SecretsConst {
+
+
+    public static final String YOS_ACCESS_KEY = "your_ACCESS_KEY_value";
+    public static final String YOS_SECRET_KEY = "your_SECRET_KEY_value";
+
+   public static final String YOS_BUCKET_PATH = "s3a://csv-test/"; //"s3a://csv/";
+
+    public static final String POSTGRE_USER = "your_POSTGRE_USER_value";
+    public static final String POSTGRE_PWD = "your_POSTGRE_PWD_value";
+    public static final String JDBC_PROVIDER_URL = "jdbc:postgresql://<your_postgre_host_name>:6432/CurrentStatus?&targetServerType=master&ssl=true&sslmode=require";
+
+}
